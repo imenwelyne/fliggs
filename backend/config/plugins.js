2abe5da6d7621db0080119a3e67fd60b07229233
mraddadi <mraddadi@welyne.com>
2023-11-13 17:16:32 +0100

    remove sitemap

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,17 +1,5 @@
 module.exports = ({ env }) => ({
   seo: {
     enabled: true,
-  },
-  'sitemap': {
-    enabled: true,
-    config: {
-      cron: '0 0 0 * * *',
-      limit: 45000,
-      xsl: true,
-      autoGenerate: true,
-      caching: true,
-      allowedFields: ['id', 'uid'],
-      excludedTypes: [],
-    },
-  },
+  }
 });
